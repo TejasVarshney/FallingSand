6a9058ede048ce74094c6544270149be792b2461
Tejas Varshney <tejas.24bcs10313@sst.scaler.com>
2025-01-22 17:58:24 +0530

    Changing color on mouseover

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 let container = document.querySelector(".container")
-let size = 100
-let bxSize = 2
+let size = 50
+let bxSize = 8
+let clockSpeed = 2
 
 function createDisplay() {
-    for(let i = 0; i<size/bxSize; i++) {
+    for(let i = 0; i<size; i++) {
         let row = document.createElement("div")
         row.setAttribute("class", `row${i+1}`)
-        for(let j = 0; j<size/bxSize; j++) {
+        
+        for(let j = 0; j<size; j++) {
             let bx = document.createElement("div")
             bx.setAttribute("class", "box")
+            bx.style.height = `${bxSize}px`
+            bx.style.width = `${bxSize}px`
+            
             row.appendChild(bx)
         }
+        
         container.appendChild(row)
     }
 }
+
+container.addEventListener("mouseover", (e) => {
+    if(e.target.classList.contains("box")) {
+        e.target.classList.add("falling")
+        e.target.style.height = `${bxSize}px`
+        e.target.style.width = `${bxSize}px`
+    }
+})
+
+createDisplay()
